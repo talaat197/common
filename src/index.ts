1e5dd561da42216923b2c908e6e9b7b2d5e92aba
Talaat Medhat <talaat.medhat@musala.com>
2022-04-10 18:05:42 +0200

    add order enum types

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,6 @@ export * from './events/base-publisher'
 export * from './events/subjects'
 export * from './events/ticket-created-event'
 export * from './events/ticket-updated-event'
+export * from './events/types/order-status'
 
 
