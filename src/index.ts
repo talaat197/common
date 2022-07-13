1eed2bd6d1df8578c7e3f1739602e6215354efc4
Talaat Medhat <talaat.medhat@musala.com>
2022-07-13 17:20:21 +0200

    fix import ticket updated event

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,10 @@ export * from './middleware/validate-request'
 export * from './events/base-listener'
 export * from './events/base-publisher'
 export * from './events/subjects'
+
 export * from './events/ticket-created-event'
+export * from './events/ticket-updated-event'
+
 export * from './events/order-created-event'
 export * from './events/order-cancelled-event'
 
